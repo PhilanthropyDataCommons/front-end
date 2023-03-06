f49fdb9c465910d859252911b09dd7ef490d0d8c
Jason Owen <jasonaowen@opentechstrategies.com>
2023-03-06 13:21:08 -0800

    Extract Placeholder component
    Move the placeholder content to its own component, in order to set the
    stage for adding routing.
    
    Issue #10 Scaffold routing and page construction

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,12 @@
 import React from 'react';
 import { Header } from './Header';
-import logo from './react-logo.svg';
+import { Placeholder } from './Placeholder';
 import './App.css';
 
 const App = () => (
   <div className="App">
     <Header />
-    <main>
-      <img src={logo} className="React-logo" alt="logo" />
-      <h1>
-        Data Viewer
-      </h1>
-      <p>
-        <a
-          className="App-link"
-          href="https://github.com/PhilanthropyDataCommons/data-viewer/"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Repository
-        </a>
-      </p>
-      <p>
-        <a
-          className="App-link"
-          href="https://philanthropydatacommons.org/"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Website
-        </a>
-      </p>
-    </main>
+    <Placeholder />
   </div>
 );
 
---
+++ src/Placeholder.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import logo from './react-logo.svg';
+
+const Placeholder = () => (
+  <main>
+    <img src={logo} className="React-logo" alt="logo" />
+    <h1>
+      Data Viewer
+    </h1>
+    <p>
+      <a
+        className="App-link"
+        href="https://github.com/PhilanthropyDataCommons/data-viewer/"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        Repository
+      </a>
+    </p>
+    <p>
+      <a
+        className="App-link"
+        href="https://philanthropydatacommons.org/"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        Website
+      </a>
+    </p>
+  </main>
+);
+
+export { Placeholder };
