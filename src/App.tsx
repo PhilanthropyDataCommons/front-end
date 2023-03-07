0acb246fbf9055d6a79ce97ee8fda099bb75d97e
Jason Owen <jasonaowen@opentechstrategies.com>
2023-03-06 16:36:53 -0800

    Add routing
    Install react-router[1], and set up a few routes: the existing
    placeholder content, and a 404 error page.
    
    [1] https://reactrouter.com/en/main
    
    Issue #10 Scaffold routing and page construction

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import { Header } from './Header';
+import { NotFound } from './NotFound';
 import { Placeholder } from './Placeholder';
 import './App.css';
 
 const App = () => (
   <div className="App">
     <Header />
-    <Placeholder />
+    <Routes>
+      <Route path="/" element={<Placeholder />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   </div>
 );
 
