96ea1f18d0629ea0e57d4f86dd246cac95133fec
Justin Reese <justin@justinreese.com>
2023-03-10 16:10:49 -0600

    Add sample Home link to navbar
    Demonstrate how we would add a standard page link (non-button) to the
    navbar. We will not actually want a Home link in the final delivery.
    
    Issue #12 Create basic interface components

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { HomeIcon } from '@heroicons/react/24/solid';
 import { User } from './User';
 
 const Navbar = () => (
   <nav className="App-navbar">
     <ul>
+      <li>
+        <NavLink to="/" className="App-navbar__item">
+          <HomeIcon />
+          Home
+        </NavLink>
+      </li>
       <li>
         <User />
       </li>
