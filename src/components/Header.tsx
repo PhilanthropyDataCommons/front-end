90b7fbe55cb069709161098c3f6b79b5ac6e7ff9
Justin Reese <justin@justinreese.com>
2023-03-09 20:11:47 -0600

    Rename logo to pdc-logo for clarity
    Issue #12 Create basic interface components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { User } from './User';
-import logo from '../images/logo.png';
+import logo from '../images/pdc-logo.png';
 
 const Header = () => (
   <header className="App-header">
