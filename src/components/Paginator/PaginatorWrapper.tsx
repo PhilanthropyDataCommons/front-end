5f8929e756e994ade2f6bcf912d6c5122cdb6b73
Justin Reese <justin@justinreese.com>
2023-03-30 16:32:02 -0500

    Add Paginator component
    This commit adds the `Paginator` component and its story, as well as the
    `PaginatorWrapper` that we use when actually placing the paginator on a
    real body.
    
    There are also two utility methods that could easily be moved to a
    utility file, but we don’t yet have a pattern for that.
    
    Issue #15 Implement pagination on proposal lists

---
+++ src/components/Paginator/PaginatorWrapper.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface PaginatorWrapperProps {
+	children: React.ReactNode;
+	className?: string;
+}
+
+export const PaginatorWrapper = ({
+	children,
+	className = '',
+}: PaginatorWrapperProps) => (
+	<div className={`paginator-wrapper ${className}`.trim()}>{children}</div>
+);
---
+++ src/components/Paginator/index.ts
@@ -0,0 +1,5 @@
+import { Paginator } from './Paginator';
+import { PaginatorButton } from './PaginatorButton';
+import { PaginatorWrapper } from './PaginatorWrapper';
+
+export { Paginator, PaginatorButton, PaginatorWrapper };
