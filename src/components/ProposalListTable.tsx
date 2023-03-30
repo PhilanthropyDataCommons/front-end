50d8742e0880767a9a80a503dfb6036d17e272fd
Justin Reese <justin@justinreese.com>
2023-03-30 16:07:50 -0500

    Add ProposalListTable component and story
    A `ProposalListTable` generates a table from an array of `proposals`.
    Each proposal is an object with the field `shortCode` as the key and the
    array of values that the proposal has for that field as the value. (Yes,
    the PDC supports multiple values for each field.)
    
    Two additional props:
    
    1. `fieldNames` map shortCodes to labels, for generating the header row.
    2. `columns` defines which fields we’re showing and dictates their
       order, as it’s just an array of `shortCodes`.
    
    This component does not have any awareness of pagination, as that will
    be the responsible of a parent component or page. It will, eventually,
    need to understand field visibility (#18) and sort order (#16).
    
    This commit also includes a Storybook story.
    
    Issue #14 Create proposals table page
    
    Co-authored-by: Jason Owen <jasonaowen@opentechstrategies.com>

---
+++ src/components/ProposalListTable.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import {
+  Table,
+  TableHead,
+  ColumnHead,
+  TableBody,
+  TableRow,
+  RowCell,
+} from './Table';
+
+interface ProposalListTableRowProps {
+  columns: string[];
+  proposal: {
+    id: string;
+    values: Record<string, string[]>;
+  };
+}
+
+const ProposalListTableRow = ({
+  proposal,
+  columns,
+}: ProposalListTableRowProps) => {
+  const navigate = useNavigate();
+
+  const handleRowClick = () => {
+    navigate(`/proposal/${proposal.id}`);
+  };
+
+  return (
+    <TableRow onClick={handleRowClick}>
+      {/* Iterate over columns to ensure order. */}
+      {columns.map((shortCode) => (
+        <RowCell key={shortCode}>
+          {proposal.values[shortCode]}
+        </RowCell>
+      ))}
+    </TableRow>
+  );
+};
+
+interface ProposalListTableProps {
+  columns: string[];
+  fieldNames: Record<string, string>;
+  proposals: {
+    id: string;
+    values: Record<string, string[]>;
+  }[];
+}
+
+export const ProposalListTable = ({
+  fieldNames,
+  proposals,
+  columns,
+}: ProposalListTableProps) => (
+  <Table>
+    <TableHead fixed>
+      <TableRow>
+        {columns.map((shortCode) => (
+          <ColumnHead key={shortCode}>
+            {fieldNames[shortCode]}
+          </ColumnHead>
+        ))}
+      </TableRow>
+    </TableHead>
+    <TableBody>
+      {proposals.map((proposal, index) => (
+        <ProposalListTableRow
+          key={index} // eslint-disable-line react/no-array-index-key
+          columns={columns}
+          proposal={proposal}
+        />
+      ))}
+    </TableBody>
+  </Table>
+);
