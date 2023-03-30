a90a152180bd77f7b60afdab58b6cca9a1cd2e19
Justin Reese <justin@justinreese.com>
2023-03-30 15:48:35 -0500

    Add support for clickable table rows
    Some table rows will need to be… clickable! E.g., clicking on them will
    perform some action like navigating to an entity detail page.
    
    When `onClick` is passed to a `TableRow`, we apply it (of course) and
    add some styling to indicate that it is clickable.
    
    Issue #14 Create proposals table page

--- src/components/Table/TableRow.tsx
+++ src/components/Table/TableRow.tsx
@@ -3,13 +3,18 @@ import React from 'react';
 interface TableRowProps {
   children: React.ReactNode;
   className?: string;
+  onClick?: () => void;
 }
 
 export const TableRow = ({
   children,
   className = '',
+  onClick = undefined,
 }: TableRowProps) => (
-  <tr className={className}>
+  <tr
+    className={`${className} ${onClick ? 'clickable' : ''}`.trim()}
+    onClick={onClick}
+  >
     {children}
   </tr>
 );
