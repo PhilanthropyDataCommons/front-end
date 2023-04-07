18f180c363cb1d92d4e86e8adcbeff4d188247cc
Justin Reese <justin@justinreese.com>
2023-04-07 10:44:11 -0500

    Add definition list styling and `<Dli>` component
    `<dl>` (definition list) elements are perfect for display key/value
    pairs or semantically similar (question/answer, etc.).
    
    This commit adds default styling for them, but also introduces the
    `<Dli>` component to wrap `<dt>`/`<dd>` pairs for better styling and
    semantics. This has been valid HTML since at least 2019.[1]
    
    [1] https://stackoverflow.com/a/18269134

---
+++ src/components/Dli.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+interface DliProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+/**
+ * Component used in `<dl>` to wrap `<dt>` and `<dd>` elements, e.g.:
+ * ```html
+ * <dl>
+ *   <Dli>
+ *     <dt />
+ *     <dd />
+ *   </Dli>
+ * </dl>
+ * ```
+ * Since this isn't a native element in HTML, this virtually creates it as a DIV.
+ */
+export const Dli = ({
+  children,
+  className = '',
+}: DliProps) => (
+  <div className={`dli ${className}`.trim()}>{children}</div>
+);
