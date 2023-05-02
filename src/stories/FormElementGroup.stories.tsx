33a597f1b20e7a5c1ae372fffd8096460fb41266
Justin Reese <justin@justinreese.com>
2023-05-01 21:50:08 -0500

    Add FormElementGroup component
    Sometimes we want form elements (buttons, inputs) to appear collected
    together in a group, such as a text input with a conjoined submit
    button.
    
    This commit adds a `FormElementGroup` component that allows for this,
    with both horizontal (default) and vertical variants.

---
+++ src/stories/FormElementGroup.stories.tsx
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import React from 'react';
+import { FormElementGroup } from '../components/FormElementGroup';
+import { Button } from '../components/Button';
+
+const meta = {
+  component: FormElementGroup,
+  tags: ['autodocs'],
+} satisfies Meta<typeof FormElementGroup>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: [
+      <Button>Button 1</Button>,
+      <Button>Button 2</Button>,
+    ],
+  },
+};
+
+export const Vertical: Story = {
+  args: {
+    direction: 'vertical',
+    children: [
+      <Button>Button 1</Button>,
+      <Button>Button 2</Button>,
+    ],
+  },
+};
