33fa1d81476138a5d09cb8a780a6f66c227ee515
Justin Reese <justin@justinreese.com>
2023-06-02 04:01:03 -0500

    Scroll active proposal into view in proposal list
    This ensures the user sees the active proposal on initial page load,
    giving them an understanding of the geography of the list.
    
    Actually scrolls to the proposal’s previous sibling (if it exists), to
    signal the user that “there are more proposals up this way”.
    
    Issue #174 Highlight current proposal in sidebar list

--- src/components/ProposalListGrid.tsx
+++ src/components/ProposalListGrid.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ProposalListGridItem } from './ProposalListGridItem';
 import { DataViewerProposal } from '../interfaces/DataViewerProposal';
 
@@ -10,14 +10,38 @@ interface ProposalListGridProps {
 export const ProposalListGrid = ({
   proposals,
   activeProposalId = undefined,
-}: ProposalListGridProps) => (
-  <div className="proposal-list-grid">
-    {proposals.map((proposal, index) => (
-      <ProposalListGridItem
-        key={index} // eslint-disable-line react/no-array-index-key
-        proposal={proposal}
-        active={activeProposalId === proposal.id}
-      />
-    ))}
-  </div>
-);
+}: ProposalListGridProps) => {
+  const listGridRef = React.useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    // We want the active proposal to be visible in the sidebar
+    // when the page renders, so the user knows "where they are".
+    //
+    // If we just scrolled the active proposal into view, however,
+    // the user might not realize there are more proposals
+    // "above" the active one; thus, we scroll to the previous element.
+    // If it doesn't exist -- i.e., if the first proposal is active --
+    // then we can noop since it's visible by default anyway.
+    //
+    // Note that we only run this on initial page render,
+    // not every time the active proposal changes.
+    // This is because we only need to orient the user on load;
+    // after that, their clicks run the show.
+    listGridRef.current?.querySelector('.active')?.previousElementSibling?.scrollIntoView();
+  }, []);
+
+  return (
+    <div
+      ref={listGridRef}
+      className="proposal-list-grid"
+    >
+      {proposals.map((proposal, index) => (
+        <ProposalListGridItem
+          key={index} // eslint-disable-line react/no-array-index-key
+          proposal={proposal}
+          active={activeProposalId === proposal.id}
+        />
+      ))}
+    </div>
+  );
+};
