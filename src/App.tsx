44a8beca0eb9f8125d2647ac3da54b4ca4882780
Jason Owen <jasonaowen@opentechstrategies.com>
2023-11-15 16:55:34 -0800

    Migrate 404 route to finish migration
    Complete the migration to the new react-router data router by moving the
    last remaining route, the 404 handler, to the data router. Delete the
    temporary, now-empty <Root> component.
    
    - https://reactrouter.com/en/main/upgrading/v6-data#start-lifting-routes-and-leveraging-the-data-apis

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
 import React from 'react';
-import {
-  createBrowserRouter, Route, Routes, RouterProvider,
-} from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Layout } from './Layout';
 import { NotFound } from './pages/NotFound';
 import { ProposalDetail } from './pages/ProposalDetail';
 import { ProposalList } from './pages/ProposalList';
 import { Landing } from './pages/Landing';
 import './App.css';
-
-const Root = () => (
-  <Routes>
-    <Route path="*" element={<NotFound />} />
-  </Routes>
-);
 
 const router = createBrowserRouter([
   {
@@ -22,7 +14,7 @@ const router = createBrowserRouter([
       { path: '/', Component: Landing },
       { path: '/proposals/:proposalId/provider?/:provider?', Component: ProposalDetail },
       { path: '/proposals', Component: ProposalList },
-      { path: '*', Component: Root },
+      { path: '*', Component: NotFound },
     ],
   },
 ]);
