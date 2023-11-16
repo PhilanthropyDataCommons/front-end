a3c007f20a6b3221375af7f4a80d4d7cefc43e25
Jason Owen <jasonaowen@opentechstrategies.com>
2023-11-15 16:43:14 -0800

    Use createBrowserRouter
    The new approach react-router recommends for configuring routes is via a
    static object, rather than as part of the React component tree. Create a
    new data router with a single route which contains all the old <Routes>.
    
    - https://reactrouter.com/en/main/routers/create-browser-router
    - https://reactrouter.com/en/main/routers/router-provider
    - https://reactrouter.com/en/main/upgrading/v6-data#add-routerprovider-with-a-root-splat-route

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {
+  createBrowserRouter, Route, Routes, RouterProvider,
+} from 'react-router-dom';
 import { Layout } from './Layout';
 import { NotFound } from './pages/NotFound';
 import { ProposalDetail } from './pages/ProposalDetail';
@@ -7,17 +9,23 @@ import { ProposalList } from './pages/ProposalList';
 import { Landing } from './pages/Landing';
 import './App.css';
 
+const Root = () => (
+  <Routes>
+    <Route element={<Layout />}>
+      <Route path="/" element={<Landing />} />
+      <Route path="/proposals/:proposalId/provider?/:provider?" element={<ProposalDetail />} />
+      <Route path="/proposals" element={<ProposalList />} />
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  </Routes>
+);
+
+const router = createBrowserRouter([
+  { path: '*', Component: Root },
+]);
+
 const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<Landing />} />
-        <Route path="/proposals/:proposalId/provider?/:provider?" element={<ProposalDetail />} />
-        <Route path="/proposals" element={<ProposalList />} />
-        <Route path="*" element={<NotFound />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
+  <RouterProvider router={router} />
 );
 
 export { App };
