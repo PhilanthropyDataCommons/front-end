d0248d08d2a4d07afb6f2e950b03b0210d5cb9e8
Justin Reese <justin@justinreese.com>
2023-11-28 18:27:58 -0600

    Add link to Add Data page to navbar
    In order to ensure the link only shows up for authenticated users, this
    commit also imports the `useOidc` hook and uses the `isAuthenticated`
    property. Consequently, the diff looks huge since we have to re-nest the
    entire render method.
    
    Issue #410 Add navbar link to bulk upload page

--- src/components/AppNavbar.tsx
+++ src/components/AppNavbar.tsx
@@ -1,40 +1,53 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { useOidc } from '@axa-fr/react-oidc';
 import { BookOpenIcon, EnvelopeIcon } from '@heroicons/react/24/outline';
-import { TableCellsIcon } from '@heroicons/react/24/solid';
+import { SquaresPlusIcon, TableCellsIcon } from '@heroicons/react/24/solid';
 import { User } from './User';
 
-const AppNavbar = () => (
-  <nav className="App-navbar">
-    <ul>
-      <li>
-        <NavLink to="/proposals" className="App-navbar__item">
-          <TableCellsIcon />
-          Dashboard
-        </NavLink>
-      </li>
-      {process.env.REACT_APP_SHOW_STORYBOOK === 'true' && (
+const AppNavbar = () => {
+  const { isAuthenticated } = useOidc();
+
+  return (
+    <nav className="App-navbar">
+      <ul>
         <li>
-          <a href="/storybook" className="App-navbar__item">
-            <BookOpenIcon />
-            Storybook
+          <NavLink to="/proposals" className="App-navbar__item">
+            <TableCellsIcon />
+            Dashboard
+          </NavLink>
+        </li>
+        {isAuthenticated && (
+          <li>
+            <NavLink to="/add-data" className="App-navbar__item">
+              <SquaresPlusIcon />
+              Add Data
+            </NavLink>
+          </li>
+        )}
+        {process.env.REACT_APP_SHOW_STORYBOOK === 'true' && (
+          <li>
+            <a href="/storybook" className="App-navbar__item">
+              <BookOpenIcon />
+              Storybook
+            </a>
+          </li>
+        )}
+        <li>
+          <a
+            href="mailto:info@philanthropydatacommons.org?Subject=Feedback%20on%20the%20Philanthropy%20Data%20Commons"
+            className="App-navbar__item"
+          >
+            <EnvelopeIcon />
+            Feedback
           </a>
         </li>
-      )}
-      <li>
-        <a
-          href="mailto:info@philanthropydatacommons.org?Subject=Feedback%20on%20the%20Philanthropy%20Data%20Commons"
-          className="App-navbar__item"
-        >
-          <EnvelopeIcon />
-          Feedback
-        </a>
-      </li>
-      <li>
-        <User />
-      </li>
-    </ul>
-  </nav>
-);
+        <li>
+          <User />
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export { AppNavbar };
