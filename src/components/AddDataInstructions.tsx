6334eadc506548b6d603734dca8f16d2e3811bec
Jason Owen <jasonaowen@opentechstrategies.com>
2023-11-30 18:13:37 -0800

    Add main instructions on bulk uploading
    Add the text that @reefdog wrote describing the use of this page.
    
    I spent a few minutes looking into rendering Markdown here, but I don't
    think we have enough here to justify the added complexity, so here's
    some hand-crafted HTML/JSX.
    
    This is a second use of the `REACT_APP_API_URL` environment variable,
    which I introduced in commit b9a6b483f9b8ed79019a227a6baee039ece2fc4e:
    
        fetch data from the API - whose location must be configured via an
        environment variable
    
    I am not using the require-env-variable library here because the error
    it throws is at runtime rather than build time.
    
    https://github.com/PhilanthropyDataCommons/front-end/wiki/Bulk-Upload-%E2%80%90-Instructional-Copy
    
    Issue #417 Add bulk upload instructional copy and link CSV template
    
    Co-authored-by: Justin Reese <justin@justinreese.com>

--- src/components/AddDataInstructions.tsx
+++ src/components/AddDataInstructions.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import './AddDataInstructions.css';
 import { Panel, PanelBody } from './Panel';
 
+const apiUrl = new URL('/', process.env.REACT_APP_API_URL);
+const bulkUploadTemplateUrl = new URL('/static/bulkUpload.csv', apiUrl);
+
 interface AddDataInstructionsProps {
   children?: React.ReactNode;
 }
@@ -11,7 +14,54 @@ export const AddDataInstructions = ({
 }: AddDataInstructionsProps) => (
   <Panel>
     <PanelBody>
-      <p>TODO: add data instructions</p>
+      <article className="add-data-instructions">
+        <h3>Add data to the PDC</h3>
+
+        <p>
+          <b>Adding data via the bulk uploader</b>
+          {' '}
+          is drag-and-drop easy:
+        </p>
+
+        <ol>
+          <li>
+            Create a CSV of your data with base field keys as the first row
+            (see table below). If you want, you can start with
+            {' '}
+            <a href={bulkUploadTemplateUrl.toString()}>
+              this template of recommended base fields
+            </a>
+            .
+          </li>
+          <li>
+            Upload your CSV in the sidebar to the left.
+          </li>
+        </ol>
+
+        <p>
+          Note that you cannot create new base fields via the bulk uploader.
+          Any columns in your file that don’t match an existing base field will
+          cause an error.
+        </p>
+
+        <p>
+          <b>Adding data via the API</b>
+          {' '}
+          allows the most control. You can create new opportunities,
+          add applications, register new base fields, and more.
+          To use the API, request an API key from
+          {' '}
+          <a href="mailto:info@philanthropydatacommons.org?Subject=PDC%20API%20key%20request">
+            info@philanthropydatacommons.org
+          </a>
+          {' '}
+          and then visit the documentation site at
+          {' '}
+          <a href={apiUrl.toString()}>{apiUrl.hostname}</a>
+          .
+        </p>
+      </article>
+
       {children}
     </PanelBody>
   </Panel>
