8d33bbbf59d6f71c748524cc986a44045a97fc20
Justin Reese <justin@justinreese.com>
2024-02-20 23:02:50 -0600

    Add Developers menu to site nav
    Issue #559 Create Developer menu in navbar

--- src/components/AppNavbar.tsx
+++ src/components/AppNavbar.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useOidc } from '@axa-fr/react-oidc';
-import { BookOpenIcon, EnvelopeIcon } from '@heroicons/react/24/outline';
-import { SquaresPlusIcon, TableCellsIcon } from '@heroicons/react/24/solid';
+import { EnvelopeIcon } from '@heroicons/react/24/outline';
+import {
+  CommandLineIcon,
+  SquaresPlusIcon,
+  TableCellsIcon,
+} from '@heroicons/react/24/solid';
 import { User } from './User';
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownMenuLink,
+} from './Dropdown';
 
 const AppNavbar = () => {
   const { isAuthenticated } = useOidc();
@@ -25,14 +35,36 @@ const AppNavbar = () => {
             </NavLink>
           </li>
         )}
-        {process.env.REACT_APP_SHOW_STORYBOOK === 'true' && (
-          <li>
-            <a href="/storybook" className="App-navbar__item">
-              <BookOpenIcon />
-              Storybook
-            </a>
-          </li>
-        )}
+        <li>
+          <Dropdown>
+            <DropdownTrigger type="navbar-item">
+              <CommandLineIcon />
+              Developers
+            </DropdownTrigger>
+            <DropdownMenu align="right">
+              <DropdownMenuLink to="https://api.philanthropydatacommons.org">
+                <div className="title">API Documentation</div>
+                <div className="description">Read the Swagger spec to interact with the PDC via our API.</div>
+              </DropdownMenuLink>
+              <DropdownMenuLink to="https://www.npmjs.com/package/@pdc/sdk">
+                <div className="title">TypeScript SDK</div>
+                <div className="description">
+                  Develop with our TypeScript SDK using
+                  {' '}
+                  <code>@pdc/sdk</code>
+                  {' '}
+                  from NPM.
+                </div>
+              </DropdownMenuLink>
+              {process.env.REACT_APP_SHOW_STORYBOOK === 'true' && (
+                <DropdownMenuLink to="/storybook" reloadDocument>
+                  <div className="title">Storybook</div>
+                  <div className="description">View our UI component library. (Only relevant to PDC front end developers.)</div>
+                </DropdownMenuLink>
+              )}
+            </DropdownMenu>
+          </Dropdown>
+        </li>
         <li>
           <a
             href="mailto:info@philanthropydatacommons.org?Subject=Feedback%20on%20the%20Philanthropy%20Data%20Commons"
