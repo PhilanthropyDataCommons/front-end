5c2982c64c945f3096819be4195366e1437e43d4
Justin Reese <justin@justinreese.com>
2024-04-16 14:49:35 -0500

    Add public data banner atop site
    Reconfigure the app layout to support an optional banner atop the page.
    For now, this banner is only used to alert public users that they may be
    viewing limited data, and prompts them to log in. In future, we may
    have additional uses for the banner.
    
    In order to support this, the layout changes required some weirdly high
    touches to the grid layout, such as moving padding off the `.App` shell
    and onto individual main grid elements.
    
    Issue #571 Add "you're seeing limited data" banner to public views

---
+++ src/components/AppBanner.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import { useOidc } from '@axa-fr/react-oidc';
+import { Button } from './Button';
+import { getLogger } from '../logger';
+
+const logger = getLogger('<AppBanner>');
+
+const AppBanner = () => {
+	const location = useLocation();
+	const { isAuthenticated, login } = useOidc();
+
+	const isHomePage = location.pathname === '/';
+	const showBanner = !isAuthenticated && !isHomePage;
+
+	return showBanner ? (
+		<div className="App-banner">
+			You are viewing limited public data.{' '}
+			<Button
+				linkStyle
+				onClick={() => {
+					login(location.pathname).catch(logger.error);
+				}}
+			>
+				Log in to view more.
+			</Button>
+		</div>
+	) : null;
+};
+
+export { AppBanner };
