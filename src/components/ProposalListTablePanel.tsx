7764a8cebbb6c5069bd9b771ec9f562218ff119d
Justin Reese <justin@justinreese.com>
2024-04-23 01:26:08 -0500

    Convert Proposals wrap button to a ToggleSwitch
    In preparation for adding the “Show only my proposals” toggle, we want
    to use a ToggleSwitch for the FKA “Toggle wrapping”.
    
    Issue #620 Design ability to filter proposals by "mine"

--- src/components/ProposalListTablePanel.tsx
+++ src/components/ProposalListTablePanel.tsx
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
-import { Bars3BottomLeftIcon } from '@heroicons/react/24/solid';
 import { FrontEndProposal } from '../interfaces/FrontEndProposal';
 import { Panel, PanelHeader, PanelBody, PanelActions } from './Panel';
 import { ProposalListTable } from './ProposalListTable';
-import { Button } from './Button';
 import { Search } from './Search';
+import { ToggleSwitch } from './ToggleSwitch';
 
 interface ProposalListTablePanelProps {
 	fieldNames: Record<string, string>;
@@ -23,7 +22,7 @@ export const ProposalListTablePanel = ({
 }: ProposalListTablePanelProps) => {
 	const [wrap, setWrap] = useState(false);
 
-	const handleWrapClick = () => setWrap((previous) => !previous);
+	const handleWrapChange = () => setWrap((previous) => !previous);
 
 	const hasProposals = proposals.length > 0;
 	const hasSearchQuery = searchQuery !== '';
@@ -43,11 +42,10 @@ export const ProposalListTablePanel = ({
 					<Search onSearch={onSearch} initialQuery={searchQuery} />
 				</PanelActions>
 				{hasProposals && (
-					<PanelActions>
-						<Button onClick={handleWrapClick} color={wrap ? 'blue' : 'gray'}>
-							<Bars3BottomLeftIcon className="icon" />
-							Toggle wrapping
-						</Button>
+					<PanelActions className="switches">
+						<ToggleSwitch checked={wrap} onChange={handleWrapChange}>
+							Wrap long text
+						</ToggleSwitch>
 					</PanelActions>
 				)}
 			</PanelHeader>
