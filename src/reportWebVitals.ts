4d07fe9359cd31d8a6a0994b1a520ac52eefc5a4
Justin Reese <justin@justinreese.com>
2024-05-20 07:48:38 -0500

    Replace removed Web Vitals methods
    Web Vitals v3 deprecated several methods and types, and v4 fully removed
    them. This commit adapts our code to the breaking changes.
    
    Note that the breaking removal of the `ReportHandler` type has been
    complained about[1] and may return in deprecated form[2], but we should
    go ahead and replace it anyway with the suggested inline typedef.
    
    [1] https://github.com/GoogleChrome/web-vitals/issues/482
    [2] https://github.com/GoogleChrome/web-vitals/pull/483
    
    PR #745 Bump web-vitals from 3.5.2 to 4.0.0

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,16 +1,17 @@
-import { getCLS, getFID, getFCP, getLCP, getTTFB } from 'web-vitals';
-import type { Metric, ReportHandler } from 'web-vitals';
+import { onCLS, onFID, onFCP, onLCP, onTTFB } from 'web-vitals';
+import type { Metric } from 'web-vitals';
 
 import { getLogger } from './logger';
 
 const reportWebVitals = () => {
 	const logger = getLogger('web-vitals');
-	const reportHandler: ReportHandler = (metric: Metric) => logger.info(metric);
-	getCLS(reportHandler);
-	getFID(reportHandler);
-	getFCP(reportHandler);
-	getLCP(reportHandler);
-	getTTFB(reportHandler);
+	const reportHandler: (metric: Metric) => void = (metric: Metric) =>
+		logger.info(metric);
+	onCLS(reportHandler);
+	onFID(reportHandler);
+	onFCP(reportHandler);
+	onLCP(reportHandler);
+	onTTFB(reportHandler);
 };
 
 export { reportWebVitals };
