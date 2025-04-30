2fc8010a53356f0d05bb5efe14875403f08b3717
Harry Minsky <harry.minsky@gmail.com>
2025-04-30 19:51:27 -0400

    Add in minimal vue application and set react code aside
    This commit introduces a minimal VueJs application to the repository,
    preserving as much editing and linting configuration as possible, and
    sets the existing react code aside, to be used as a reference for
    rebuilding the front-end in Vue. Thus far, no dependencies have
    been removed, but a slew of vue related dependencies have been
    added, namely the vue package itself and the official vue
    eslint and typescript configurations. Since we are no longer
    developing the react codebase, CI/CD is now only pointing at
    the new Vue files. Subsequent commits will involve the rebuilding
    of each of our existing react components in the vue-est way possible,
    and re-establishing our routing and authentication system.
    
    Issue #1009 Migrate front-end to Vue

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+<template>
+	<header>
+		<h1 class="welcome-message">Welcome to the PDC</h1>
+	</header>
+</template>
+
+<style scoped>
+.welcome-message {
+	color: rgb(176, 65, 65);
+}
+</style>
---
+++ src/placeholder.test.ts
@@ -0,0 +1,5 @@
+import { expect, test } from 'vitest';
+
+test('Placeholder test to ensure vitest is working', () => {
+	expect(true).toBe(true);
+});
