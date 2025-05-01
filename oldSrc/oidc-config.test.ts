c345b17f14bfa2956dac32782251433c8cd0ba43
Harry Minsky <harry.minsky@gmail.com>
2025-04-30 23:57:43 -0400

    Add Minimal router setup
    This commit minimally adds in the Vue router, the official Vue routing
    package, which we will need not only for handling routing but likely
    handling authentication.
    
    Issue #1009 Migrate front-end to Vue

--- oldSrc/oidc-config.test.ts
+++ oldSrc/oidc-config.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest';
-import { getConfiguration } from '../src/tests/oidc-config';
+import { getConfiguration } from './oidc-config';
 
 test('configuration fails with missing authority', () => {
 	expect(() => getConfiguration(undefined, 'client-id')).toThrow(/authority/);
---
+++ src/views/LandingView.vue
@@ -0,0 +1,10 @@
+<template>
+	<h1>Welcome to the Philanthropy Data Commons.</h1>
+</template>
+
+<style scoped>
+h1 {
+	font-weight: 600;
+	color: red;
+}
+</style>
