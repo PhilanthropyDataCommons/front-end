76c02ab5751bee9490a87e22934d73e89cc34371
Harry Minsky <harry.minsky@gmail.com>
2025-04-30 23:57:43 -0400

    Add Minimal router setup
    This commit minimally adds in the Vue router, the official Vue routing
    package, which we will need not only for handling routing but likely
    handling authentication.
    
    Issue #1009 Migrate front-end to Vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,7 @@
 <script setup lang="ts"></script>
 <template>
-	<header>
-		<h1 class="welcome-message">Welcome to the PDC</h1>
-	</header>
+	<RouterView />
 </template>
 
 <style scoped>
-.welcome-message {
-	color: rgb(176, 65, 65);
-}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { createApp } from 'vue';
+import { router } from '@/router';
 import App from '@/App.vue';
 import type { Component } from 'vue';
 
 const app = createApp(App as Component);
+
+app.use(router);
 
 app.mount('#app');
