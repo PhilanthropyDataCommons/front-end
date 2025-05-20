2299a48470223129a0a3a537fd42739ae09d42ad
Harry Minsky <harry.minsky@gmail.com>
2025-05-20 11:26:44 -0400

    Add unused components
    This commit migrates over remaining components in the react application
    that are not currently being rendered on any page. Structure has largely
    been maintained, with one exception being the ListGrid component, which
    has been split into separate components a-la Panel and Dropdown.  This
    has been done to abide by Vue's SFC philosophy, and keep consistent that
    each file should contain the code for exactly one component.  Similarly,
    one change from the existing ListGrid component is to longer accept a
    rendering function as a prop. Rather than rendering components through
    explicit function calls, the new ListGrid component can render it's
    children using the scoped-slot syntax [1].
    
    An example ProposalListGrid might look like
    ```
    <template>
     <ListGrid :items="items">
       <template #default="{item: proposal}">
        <ListGridItem>
    	{etc,etc}
        </ListGridItem>
       </template
     </ListGrid>
    <template/>
    ```
    Which would then render all items in the prop array.
    
    [1]https://vuejs.org/guide/components/slots#scoped-slots

---
+++ src/components/BaseFields.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import type { BaseField } from '@pdc/sdk';
+import {
+	TableComponent,
+	TableHead,
+	ColumnHead,
+	TableBody,
+	TableRow,
+	RowCell,
+} from './Table';
+
+interface BaseFieldsProps {
+	fields: BaseField[];
+}
+
+const { fields } = defineProps<BaseFieldsProps>();
+</script>
+
+<template>
+	<TableComponent>
+		<TableHead fixed>
+			<TableRow>
+				<ColumnHead>Key</ColumnHead>
+				<ColumnHead>Label</ColumnHead>
+				<ColumnHead>Description</ColumnHead>
+			</TableRow>
+		</TableHead>
+		<TableBody>
+			<TableRow v-for="field in fields" :key="field.id">
+				<RowCell>
+					<code className="short-code">{{ field.shortCode }}</code>
+				</RowCell>
+				<RowCell>{{ field.label }}</RowCell>
+				<RowCell>{{ field.description }}</RowCell>
+			</TableRow>
+		</TableBody>
+	</TableComponent>
+</template>
---
+++ src/components/FormElementGroup.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+interface FormElementGroupProps {
+	direction?: 'horizontal' | 'vertical';
+	className?: string;
+}
+
+const { direction = 'horizontal', className = '' } =
+	defineProps<FormElementGroupProps>();
+</script>
+
+<template>
+	<div :class="`form-element-group ${direction} ${className}`.trim()">
+		<slot></slot>
+	</div>
+</template>
+
+<style>
+.form-element-group {
+	display: flex;
+	gap: 0;
+}
+
+.form-element-group > *:is(:focus-visible, :active) {
+	z-index: 1;
+}
+
+.form-element-group.horizontal {
+	flex-direction: row;
+}
+
+.form-element-group.horizontal > *:first-child {
+	border-top-right-radius: 0;
+	border-bottom-right-radius: 0;
+}
+
+.form-element-group.horizontal > *:not(:first-child, :last-child) {
+	border-radius: 0;
+}
+
+.form-element-group.horizontal > *:not(:first-child) {
+	border-left: 0;
+}
+
+.form-element-group.horizontal > *:last-child {
+	border-top-left-radius: 0;
+	border-bottom-left-radius: 0;
+}
+
+.form-element-group.vertical {
+	flex-direction: column;
+}
+
+.form-element-group.vertical > *:first-child {
+	border-bottom-left-radius: 0;
+	border-bottom-right-radius: 0;
+}
+
+.form-element-group.vertical > *:not(:first-child, :last-child) {
+	border-radius: 0;
+}
+
+.form-element-group.vertical > *:not(:first-child) {
+	border-top: 0;
+}
+
+.form-element-group.vertical > *:last-child {
+	border-top-left-radius: 0;
+	border-top-right-radius: 0;
+}
+</style>
