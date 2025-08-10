6ac701a8d95d2129f048309ecc05f0130dcbf873
Harry Minsky <harry.minsky@gmail.com>
2025-08-10 16:51:36 +0000

    Remove unused header components and css
    This commit removes the unused header components and related css, in
    preperation for the implementation of the side panel.
    
    Issue #1063 Implement Side Panel

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -3,9 +3,6 @@ export { default as DliComponent } from './components/DliComponent.vue';
 export { default as EmailLink } from './components/EmailLink.vue';
 export { default as OffSiteLink } from './components/OffSiteLink.vue';
 export { default as UserComponent } from './components/UserComponent.vue';
-export { default as AppBanner } from './components/AppBanner.vue';
-export { default as AppNavBar } from './components/AppNavBar.vue';
-export { default as AppHeader } from './components/AppHeader.vue';
 
 export {
 	DropdownComponent,
