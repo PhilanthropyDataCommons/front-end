437e3bea1efeba4e389cf19ce43ce13ca6fc0e1d
Harry Minsky <harry.minsky@gmail.com>
2025-08-13 13:20:49 -0400

    Implement table components
    This commit adds a suite of table related components, modeled after the
    table components that we built in the previous version of this
    application for rendering data from the pdc in a row/column format. They
    are intended to be used in both the bulk-uploader and base field admin
    applications, and are extendable to any data type in the pdc we may wish
    to render as such.
    
    Column actions are intended to house future logic for sorting and
    filtering the data in tables. Those will be accomplished in a future
    commit.
    
    Issue #1068 Implement table components and table filtering functionality

---
+++ packages/components/src/components/Table/TableComponent.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+export interface TableProps {
+	className?: string;
+	/**
+	 * Table body cells won't wrap and will have a max-width,
+	 * above which the text will be hidden.
+	 */
+	truncate?: boolean;
+}
+
+const { className = '', truncate = false } = defineProps<TableProps>();
+</script>
+
+<template>
+	<table
+		:border="0"
+		:cellPadding="0"
+		:cellSpacing="0"
+		:class="`table ${truncate ? 'truncate' : ''} ${className}`.trim()"
+	>
+		<slot> </slot>
+	</table>
+</template>
+
+<style>
+:root {
+	--table--border-width: 1px;
+}
+
+.table {
+	width: 100%;
+}
+
+/* The last column should stretch to the table edge and have no right border. */
+.table th:last-child,
+.table td:last-child {
+	width: 100%;
+	border-right: none;
+}
+
+/* The last body row should have a darker bottom border. */
+.table tbody tr:last-child th,
+.table tbody tr:last-child td {
+	border-bottom: var(--table--border-width) solid var(--color--gray--medium);
+}
+
+/* Truncated tables don't wrap table body contents. */
+.table.truncate tbody td {
+	white-space: nowrap;
+	overflow: hidden;
+	text-overflow: ellipsis;
+	max-width: 30ch;
+}
+
+.table .clickable {
+	cursor: pointer;
+}
+
+.table .clickable:not(.active):hover {
+	background-color: var(--color--gray--lighter);
+}
+
+.table .active {
+	background-color: var(--color--blue--lighter);
+}
+</style>
---
+++ packages/components/src/components/Table/TableRowCell.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+export interface RowCellProps {
+	className?: string;
+}
+
+const { className = '' } = defineProps<RowCellProps>();
+</script>
+
+<template>
+	<td :class="` ${className}`.trim()">
+		<slot> </slot>
+	</td>
+</template>
+<style scoped>
+td {
+	vertical-align: top;
+	padding: var(--accessible-spacing--1x);
+	border-bottom: var(--table--border-width) solid var(--color--gray--light);
+}
+</style>
