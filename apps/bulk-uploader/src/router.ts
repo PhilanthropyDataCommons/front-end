c3ce3ede7472f28a72b737360c36b9c87163b276
Harry Minsky <harry.minsky@gmail.com>
2025-08-13 13:26:07 -0400

    Add Individual Bulk Upload View
    This commit adds the view for an individual bulkupload, which shows
    the status and further details about the upload. Currently, much
    of the desired data for the bulk-upload is not supported in the pdc,
    so we have left them as TODOs in the view itself. To that same point,
    we are also in need of an individual bulkUploads endpoint to get
    one by id. For the time being, we've implemented client-side filtering
    across all bulk-uploads, but we can change that once we've implemented
    the necessary routes in service.
    
    Issue #1066 Implement Individual Bulk Upload view

--- apps/bulk-uploader/src/router.ts
+++ apps/bulk-uploader/src/router.ts
@@ -3,10 +3,12 @@ import LandingView from './views/LandingView.vue';
 import NotFoundView from './views/NotFoundView.vue';
 import type { Router } from 'vue-router';
 import BulkUploadsView from './views/BulkUploadsView.vue';
+import BulkUploadView from './views/BulkUploadView.vue';
 
 const routes = [
 	{ path: '/', component: LandingView },
 	{ path: '/bulk-uploads', component: BulkUploadsView },
+	{ path: '/bulk-uploads/:id', component: BulkUploadView },
 	{ path: '/:pathMatch(.*)', component: NotFoundView },
 ];
 
---
+++ apps/bulk-uploader/src/views/BulkUploadView.vue
@@ -0,0 +1,155 @@
+<script setup lang="ts">
+import {
+	PanelComponent,
+	PanelBody,
+	PanelHeader,
+	PanelHeaderAction,
+	PanelHeaderActionsWrapper,
+	PanelSection,
+} from '@pdc/components';
+import BulkUploadStatus from '@/components/BulkUploadStatus.vue';
+import { RouterLink, useRoute } from 'vue-router';
+import { ArrowLeftIcon, DocumentPlusIcon } from '@heroicons/vue/24/outline';
+import { ref, onMounted, onUnmounted } from 'vue';
+import { useBulkUploads } from '../pdc-api';
+import { BulkUploadTask as BulkUploadTaskType } from '@pdc/sdk';
+import type { BulkUploadTask } from '@pdc/sdk';
+import { getLogger, localizeDateTime } from '@pdc/utilities';
+
+const logger = getLogger('BulkUploadView');
+
+const { StatusEnum: BulkUploadTaskStatus } = BulkUploadTaskType;
+const POLL_INTERVAL_MS = 2000;
+
+const {
+	params: { id: bulkUploadId },
+} = useRoute();
+const bulkUpload = ref<BulkUploadTask | undefined>(undefined);
+const { data: bulkUploads, fetchData: fetchBulkUploads } = useBulkUploads();
+let pollInterval: ReturnType<typeof setInterval> | null = null;
+
+const startPolling = (): void => {
+	if (pollInterval !== null) clearInterval(pollInterval);
+	pollInterval = setInterval(() => {
+		void (async () => {
+			if (bulkUpload.value?.status === BulkUploadTaskStatus.InProgress) {
+				try {
+					await fetchBulkUploads();
+					bulkUpload.value = bulkUploads.value?.entries.find(
+						(upload) => upload.id === Number(bulkUploadId),
+					);
+				} catch (error) {
+					logger.error({ error }, 'Error fetching bulk upload');
+				}
+			} else {
+				stopPolling();
+			}
+		})();
+	}, POLL_INTERVAL_MS);
+};
+
+const stopPolling = (): void => {
+	if (pollInterval !== null) {
+		clearInterval(pollInterval);
+		pollInterval = null;
+	}
+};
+
+onMounted(() => {
+	void (async () => {
+		await fetchBulkUploads();
+		bulkUpload.value = bulkUploads.value?.entries.find(
+			(upload) => upload.id === Number(bulkUploadId),
+		);
+
+		if (bulkUpload.value?.status === BulkUploadTaskStatus.InProgress) {
+			startPolling();
+		}
+	})();
+});
+
+onUnmounted(() => {
+	stopPolling();
+});
+</script>
+
+<template>
+	<PanelComponent v-if="bulkUpload" padded>
+		<PanelHeader>
+			<div class="bulk-upload-header">
+				<h1>BulkUpload {{ bulkUpload.id }}</h1>
+				<BulkUploadStatus :status="bulkUpload.status" />
+			</div>
+
+			<PanelHeaderActionsWrapper>
+				<PanelHeaderAction>
+					<ArrowLeftIcon class="icon" />
+					<RouterLink to="/bulk-uploads">Back to bulk uploads</RouterLink>
+				</PanelHeaderAction>
+			</PanelHeaderActionsWrapper>
+		</PanelHeader>
+		<PanelBody variant="data-panel-padded">
+			<PanelSection>
+				<template #header>
+					<h3>Upload Details</h3>
+				</template>
+				<template #content>
+					<div>
+						<div class="disabled">
+							<h4>Original File</h4>
+							<div class="file-download">
+								<DocumentPlusIcon class="icon" />
+								<p :href="bulkUpload.fileName" target="_blank">
+									TO DO: IMPLEMENT
+								</p>
+							</div>
+						</div>
+						<h4>Job ID</h4>
+						<p>{{ bulkUpload.id }}</p>
+						<h4>Timestamp</h4>
+						<p>{{ localizeDateTime(bulkUpload.createdAt) }}</p>
+						<h4>Uploader</h4>
+						<p>{{ bulkUpload.createdBy }}</p>
+					</div>
+				</template>
+			</PanelSection>
+			<PanelSection>
+				<template #header>
+					<h3>Result</h3>
+				</template>
+				<template #content>
+					<div>
+						<h4>Upload Result</h4>
+						<p><BulkUploadStatus :status="bulkUpload.status" /></p>
+						<h4>Errors Encountered</h4>
+						<p>TODO: Implement</p>
+						<h4>Proposals Added</h4>
+						<p>TODO: Implement</p>
+					</div>
+				</template>
+			</PanelSection>
+		</PanelBody>
+	</PanelComponent>
+</template>
+
+<style scoped>
+.bulk-upload-header {
+	display: flex;
+	align-items: center;
+	gap: var(--fixed-spacing--1x);
+}
+.file-download {
+	display: flex;
+	align-items: center;
+	gap: var(--fixed-spacing--1x);
+	color: var(--color--blue);
+}
+.file-download a {
+	color: var(--color--blue);
+}
+
+.disabled {
+	opacity: 0.5;
+	cursor: not-allowed;
+}
+</style>
