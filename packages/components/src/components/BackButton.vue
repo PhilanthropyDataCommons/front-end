212fd2f375e2bae1f292b4f5389db83ecffce81f
Harry Minsky <harry.minsky@gmail.com>
2025-10-06 23:44:06 -0400

    Abstract out back button component
    This commit pulls the back button into it's own component,
    as it's already used in three places, and will be used
    again in the individual base field view.
    
    Issue #1160 Add Basefield Admin edit basefield view

---
+++ packages/components/src/components/BackButton.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ArrowLeftIcon } from '@heroicons/vue/24/outline';
+import { RouterLink } from 'vue-router';
+import PanelHeaderAction from './Panel/PanelHeaderAction.vue';
+
+defineProps<{
+	to: string;
+	label: string;
+}>();
+</script>
+
+<template>
+	<PanelHeaderAction>
+		<ArrowLeftIcon class="icon" />
+		<RouterLink :to="to">{{ label }}</RouterLink>
+	</PanelHeaderAction>
+</template>
